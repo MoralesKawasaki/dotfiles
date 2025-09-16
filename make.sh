3156e80638aa8f357c719f1c721b9a03a3f75760
MoralesKawasaki <konstanty.spyrzewski@protonmail.com>
2025-09-16 20:19:47 +0200

    Fix spelling mistake make.sh

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 files="alacritty dwm dwmblocks kitty picom rofi"
-install="alacritty kitty rofi git vim feh playerctl xorg xorg-server xorg-xinit xorg-xrandr yazi pcmanfm ttf-noto-nerd xdg=user-dirs"
+install="alacritty kitty rofi git vim feh playerctl xorg xorg-server xorg-xinit xorg-xrandr yazi pcmanfm ttf-noto-nerd xdg-user-dirs"
 aur="picom-git librewolf-bin"
 
 echo -n "Installing all official and AUR packages ..."
