07188b144d40fbdbde3a2408940edf91c089186c
MoralesKawasaki <konstanty.spyrzewski@protonmail.com>
2025-09-16 19:34:23 +0200

    Copy wallpapers to proper directory

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 dir=~/dotfiles
 olddir=~/dotfiles_old
 files="alacritty dwm dwmblocks kitty picom rofi"
-install="alacritty kitty rofi git vim feh playerctl xorg xorg-server xorg-xinit xorg-xrandr yazi pcmanfm ttf-noto-nerd"
+install="alacritty kitty rofi git vim feh playerctl xorg xorg-server xorg-xinit xorg-xrandr yazi pcmanfm ttf-noto-nerd xdg=user-dirs"
 aur="picom-git librewolf-bin"
 
 echo -n "Installing all official and AUR packages ..."
@@ -35,3 +35,8 @@ ln -s $dir/starship.toml ~/.config/
 
 echo "Creating symlink to .xinitrc in home directory"
 ln -s $dir/.xinitrc ~/
+
+echo "Creating xdg directories and copying wallpapers to ~/Pictures"
+xdg-user-dirs-update
+cp $dir/Wallpapers/* ~/Pictures/
+echo "done"
